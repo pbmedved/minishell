8abefed3c0167b197ad8fc82525ebd92723a128d
AmmoMax <steelratmax@gmail.com>
2020-12-09 00:31:29 +0300

    added TODO

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iadrien <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: amayor <amayor@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 07:19:51 by iadrien           #+#    #+#             */
-/*   Updated: 2020/11/27 07:20:01 by iadrien          ###   ########.fr       */
+/*   Updated: 2020/12/08 23:31:46 by amayor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+//TODO: непонятно надо ли фиксировать global_r_code для exit
 void 			exit_handler(t_command *comm)
 {
 	if (comm->args)
