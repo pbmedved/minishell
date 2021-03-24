84d096dcb0d7e179ed8cc0c7fa0343ddea5e5de2
AmmoMax <steelratmax@gmail.com>
2021-03-25 00:19:34 +0300

    fixed leaks for exit without error code

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: amayor <amayor@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 07:19:51 by iadrien           #+#    #+#             */
-/*   Updated: 2021/03/24 23:51:51 by amayor           ###   ########.fr       */
+/*   Updated: 2021/03/25 00:18:56 by amayor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,16 @@ void		exit_handler(t_command *comm, t_vars *vars, t_exe *exe)
 	int		err_code;
 
 	if (comm->args)
-	{
-		free(vars->prompt);
-		free(vars->buff);
-		vars->buff = 0;
-		dell_all_env(&vars->env);
-		ft_putstr_fd("exit\n", 1);
 		err_code = ft_atoi(comm->args->arg);
-		dell_all_args(&comm->args);
-		free(comm->command);
-		free(comm);
-		clean_exe(exe);
-		exit(err_code);
-	}
-	exit(0);
+	err_code = 0;
+	free(vars->prompt);
+	free(vars->buff);
+	vars->buff = 0;
+	dell_all_env(&vars->env);
+	ft_putstr_fd("exit\n", 1);
+	dell_all_args(&comm->args);
+	free(comm->command);
+	free(comm);
+	clean_exe(exe);
+	exit(err_code);
 }
